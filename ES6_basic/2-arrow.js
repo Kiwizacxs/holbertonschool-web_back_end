fbad757cc839ecc427e9ed4a435c89d2df1cad55
kiwizacxs <brian.deleon2016@outlook.com>
2024-05-07 19:00:44 -0700

    rewrite to arrow function

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,7 +1,10 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  this.addNeighborhood = function add(newNeighborhood) {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
+  const addNeighborhood = (newNeighborhoods) => {
+      this.sanFranciscoNeighborhoods.push(newNeighborhoods);
+      return this.sanFranciscoNeighborhoods;
   };
+  return {
+      addNeighborhood: addNeighborhood
+}
 }
