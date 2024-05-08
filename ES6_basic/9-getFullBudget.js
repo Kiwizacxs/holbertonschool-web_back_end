81536b5f2e6ac256f09f8d39d999652aba3a6ca3
Kiwizacxs <brian.deleon2016@outlook.com>
2024-05-08 09:52:15 -0700

    full budget in ES6 syntax

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -3,7 +3,7 @@ import getBudgetObject from './7-getBudgetObject.js';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   return {
-    getIncomeInDollars: (income) => `$${budget.income}`,
-    getIncomeInEuros: (income) => `${budget.income} euros`,
+    getIncomeInDollars: () => `$${budget.income}`,
+    getIncomeInEuros: () => `${budget.income} euros`,
   };
 }
