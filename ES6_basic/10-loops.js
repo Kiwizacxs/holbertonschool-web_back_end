5942b1c0c28dfb9048103e07397a50ab8850ac4d
kiwizacxs <brian.deleon2016@outlook.com>
2024-05-12 18:50:03 -0700

    loop for in ES6 format

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const new_array = []
+  const new_array = [];
   for (const value of array) {
-    new_array.push(appendString + value)
+    new_array.push(appendString + value);
   }
   return new_array;
 }
