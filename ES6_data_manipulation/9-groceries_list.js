22f65ac2f24cbe6e0d0e2cbf3a6767f9542b68d0
Kiwizacxs <brian.deleon2016@outlook.com>
2024-07-10 06:16:00 -0700

    groceries lists

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+// 9-groceries_list.js
+
+export default function groceriesList() {
+  const groceryMap = new Map();
+
+  groceryMap.set('Apples', 10);
+  groceryMap.set('Tomatoes', 10);
+  groceryMap.set('Pasta', 1);
+  groceryMap.set('Rice', 1);
+  groceryMap.set('Banana', 5);
+
+  return groceryMap;
+}
