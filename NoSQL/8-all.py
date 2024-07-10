7b8feb9c013b9975d97e42ef493319d8494960f7
Kiwizacxs <brian.deleon2016@outlook.com>
2024-07-10 07:38:41 -0700

    lets go

---
+++ NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module that contain list_all function"""
+
+
+def list_all(mongo_collection):
+    """Function that lists all documents in a collection"""
+    My_doc = []
+    for doc in mongo_collection.find():
+        My_doc.append(doc)
+    return My_doc
