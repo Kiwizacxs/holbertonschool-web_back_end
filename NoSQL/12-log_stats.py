5a02195a7d570c14bb55a7ba2ea731fb9ecf7980
Kiwizacxs <brian.deleon2016@outlook.com>
2024-07-10 08:17:53 -0700

    lets go

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,7 +3,7 @@
 from pymongo import MongoClient
 
 def print_results(log_count, methods, status_check):
-  """function of the module"""
+    """function of the module"""
     print(f"{log_count} logs")
     print("Methods:")
     for method, count in methods.items():
