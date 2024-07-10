5a0640ff5628c2730e59f4d0000ac4264fe31ee4
Kiwizacxs <brian.deleon2016@outlook.com>
2024-07-10 06:14:46 -0700

    clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -5,7 +5,7 @@ export default function cleanSet(set, startString) {
 
   const result = [];
 
-  set.forEach(value => {
+  set.forEach((value) => {
     if (typeof value === 'string' && value.startsWith(startString)) {
       result.push(value.slice(startString.length));
     }
