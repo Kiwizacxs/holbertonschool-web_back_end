a0fa8211eb3e4cb35bedd7e73cba7659b97fca84
Kiwizacxs <brian.deleon2016@outlook.com>
2024-07-10 09:26:11 -0700

    twelve

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-"""module"""
+"""Script that provides some stats about Nginx logs stored in MongoDB."""
 from pymongo import MongoClient
 
+
 def print_results(log_count, methods, status_check):
-    """function of the module"""
-    print(f"{log_count} logs")
-    print("Methods:")
+    """Print the results."""
+    print(f"{log_count} logs\nMethods:")
     for method, count in methods.items():
         print(f"\tmethod {method}: {count}")
     print(f"{status_check} status check")
 
+
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    nginx_collection = db.nginx
+    nginx_collection = client.logs.nginx
 
     log_count = nginx_collection.count_documents({})
+
     methods = {
         'GET': nginx_collection.count_documents({'method': 'GET'}),
         'POST': nginx_collection.count_documents({'method': 'POST'}),
@@ -23,6 +24,7 @@ if __name__ == "__main__":
         'PATCH': nginx_collection.count_documents({'method': 'PATCH'}),
         'DELETE': nginx_collection.count_documents({'method': 'DELETE'}),
     }
+
     status_check = nginx_collection.count_documents({'method': 'GET', 'path': '/status'})
 
     print_results(log_count, methods, status_check)
